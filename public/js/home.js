22d3f5ecc1aa4b4373b5bda87cb574763b350b5e
Artur <akaluza26@gmail.com>
2018-05-18 17:20:38 +0200

    Add home POST /subscribe handler

---
+++ public/js/home.js
@@ -0,0 +1,30 @@
+const newsLatter = document.getElementById('newslatter');
+newsLatter.addEventListener('submit', getSubscribe);
+
+function getSubscribe(e) {
+    e.preventDefault();
+    let email = e.target.email;
+    fetch('/subscribe', {
+        method: 'POST', 
+        body: JSON.stringify({email: email.value}),
+        headers: new Headers({
+            'Content-Type': 'application/json'
+    })}).then((data) => {
+        if (data.status == 201) {
+            email.value = 'Thanks';
+            setTimeout(() => {
+                email.value = '';                
+            }, 2500);
+            
+        } else if (data.status == 406) {
+            email.value = 'email is not valid!!!';
+            setTimeout(() => {
+                email.value = '';
+            },  2500);
+        }
+    }).catch(e => {
+       console.log(e);
+    });     
+};
+
+
--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const hbs = require('hbs');
+const bodyParser = require('body-parser');
+const { isEmail } = require('validator');
 
 const {date} = require('./util/hbsObject');
 
 const app = express();
 
+// set html engine and static assets
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+// set partials location
 hbs.registerPartials(__dirname + '/views/partials')
+
+// body parser
+app.use(bodyParser.json())
+
 
 app.get('/', (req, res) => {
     res.render('home.hbs', date);
@@ -25,6 +33,15 @@ app.get('/about', (req, res) => {
 app.get('/services', (req, res) => {
     res.render('services.hbs', date);
 });
+
+app.post('/subscribe', (req, res) => {
+    if (isEmail(req.body.email)) {
+        return res.status(201).send();
+    } else {
+        return res.status(406).send();
+    }
+});
+
 
 app.listen(3000, () => {
     console.log('server is running');
